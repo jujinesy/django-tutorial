fbade4d64671ea74bd400f98d3301e07c9bd41df
jujinesy <jujinesy@gmail.com>
2014-06-29 04:48:32 +0900

    update

--- simpleboard/views.py
+++ simpleboard/views.py
@@ -6,11 +6,76 @@ from .models import Posts
 from django.db.models import F
 from django.forms import ModelForm
 
+
+import mechanize
+import urllib
+from bs4 import BeautifulSoup
+import json
+import time
+
+
+
 # Create your views here.
 def list_posts(request):
     posts = Posts.objects.order_by("-id").all()
     template = loader.get_template('list.html')
-    context = RequestContext(request, {"A":2, "posts" : posts })
+    
+
+
+    Mainlist= [];
+    Sublist= [];
+    browser = mechanize.Browser()
+    pageNum=0;
+    review_date=0;
+    review_title=0;
+    review_content=0;
+
+    while 1:
+        param = { 'reviewType': '0', 'pageNum': pageNum, 'id': 'jp.naver.line.android', 'reviewSortOrder': '0', 'xhr': '1' }
+        data = urllib.urlencode(param)
+        browser.set_handle_robots(False)
+        resp = browser.open('https://play.google.com/store/getreviews', data)
+        content = json.loads(resp.read()[5:])
+
+        if len(content) != 0 and content[0][0] != 'ger' :
+        # data3 = json.loads(data2)
+        # print type(data3) , len(data3) , type(data3[0]) , len(data3[0])
+        # print data2[0][0]
+        # print data2[0][1]
+        # print data2[0][2].encode("utf8")
+        # print data2[0][3]
+            bs5 = BeautifulSoup(content[0][2].encode("utf8"))
+            reviews = bs5.find_all("div", "single-review")
+            for review in reviews:
+                cur_date = review.find("span", "review-date")
+                body = review.find("div", "review-body")
+                
+                review_date =  cur_date.string
+                review_title = body.contents[1].string
+                review_content = body.contents[2].string
+                
+                print "Date :" , review_date
+                print "Title :" , review_title
+                print "Content :" , review_content
+                print "\n"
+
+                Sublist.append(review_date);
+                Sublist.append(review_title);
+                Sublist.append(review_content);
+
+                Mainlist.append(Sublist);
+                Sublist= [];
+
+        else:
+            print "data not exists"
+            break
+        if pageNum > 1:
+            break
+        #time.sleep(3)
+        pageNum+=1
+
+    print type(posts)
+    context = RequestContext(request, {"A":2, "posts" : posts, "Mainlist" : Mainlist })
     return HttpResponse( template.render(context))
 
 def view_post(request, post_id=None):
